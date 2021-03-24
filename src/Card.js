563913eb52078d518d07bb2287ce1ee98a11c9e6
Rahul Dubey <rahuld7219@gmail.com>
2021-03-24 13:44:57 +0530

    Add some cards with robohash api and using tachyons css

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,27 @@
 //this file name is capitalized by convention as it is a react component
 import React from 'react';
 
-function Card() {
+const Card = (props) => {
+    const { name, email, id} = props; // can be destructured directly as function parameter
 	return (
-		<h1>dummy return</h1>
+		<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+		{/* used tachyons CSS classes 
+			tc=>text center
+			dib=>display inline block
+			grow=>	
+					-moz-osx-font-smoothing:grayscale
+					backface-visibility:hidden
+					transform:translateZ(0)
+					transition:transform 0.25s ease-out
+			bw=>border width
+			shadow-5=> box-shadow:4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )
+		*/}
+			<img src={`https://robohash.org/${id}?size=200x200`} alt="a robot"/>
+			<div>
+				<h2>{name}</h2>{/*we can also write it as props.name*/}
+				<p>{email}</p>
+			</div>
+		</div>
 	);
 }
 
